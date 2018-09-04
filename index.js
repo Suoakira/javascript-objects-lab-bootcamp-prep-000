7d5140608029567783103c637680da3349a63b5c
Stephen Courtier <steves0089@gmail.com>
2018-09-04 13:27:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ let recipes = new Object();
 
 function updateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
-  return object
 }
 
 function updateObjectWithKeyAndValue(object, key, value) {
